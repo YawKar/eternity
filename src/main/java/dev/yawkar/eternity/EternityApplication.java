ec6e99d18e7c6f7c9cce658967497b9ba90487ab
yawkar <yawkarpov@gmail.com>
2022-08-18 01:54:30 +0300

    init: init project

---
+++ src/main/java/dev/yawkar/eternity/EternityApplication.java
@@ -0,0 +1,13 @@
+package dev.yawkar.eternity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EternityApplication.class, args);
+    }
+
+}
