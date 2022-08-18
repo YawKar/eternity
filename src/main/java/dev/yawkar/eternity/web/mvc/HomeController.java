e5869c6f5c803347ab9c54d622231c1977994278
yawkar <yawkarpov@gmail.com>
2022-08-18 04:50:40 +0300

    feat: add list of newsTopics on the home page

--- src/main/java/dev/yawkar/eternity/web/mvc/HomeController.java
+++ src/main/java/dev/yawkar/eternity/web/mvc/HomeController.java
@@ -1,13 +1,29 @@
 package dev.yawkar.eternity.web.mvc;
 
+import dev.yawkar.eternity.service.NewsService;
+import dev.yawkar.eternity.web.dto.NewsTopicDTO;
+import dev.yawkar.eternity.web.mapper.NewsTopicMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final NewsService newsService;
+    private final NewsTopicMapper newsMapper;
+
+    public HomeController(NewsService newsService, NewsTopicMapper newsMapper) {
+        this.newsService = newsService;
+        this.newsMapper = newsMapper;
+    }
+
     @GetMapping
-    String homepage() {
+    String homepage(Model model) {
+        List<NewsTopicDTO> newsTopics = newsService.getAllNewsSortedByTime().stream().map(newsMapper::toDTO).toList();
+        model.addAttribute("newsTopics", newsTopics);
         return "homepage";
     }
 }
