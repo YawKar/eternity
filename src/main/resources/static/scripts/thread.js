eb0525a6043e01ca1db4060e58420fc1f98a0717
yawkar <yawkarpov@gmail.com>
2022-08-20 14:48:07 +0300

    frontend(js): add additional conditions before sending the message

--- src/main/resources/static/scripts/thread.js
+++ src/main/resources/static/scripts/thread.js
@@ -7,7 +7,13 @@ const messagesEndpoint = '/api/v1/messages';
 
 document.getElementById("message-text-input").onkeydown = function (e) {
     if (e.key === "Enter") {
-        if (messageTextInput.innerText.length > 0 && messageTextInput.innerText.length < 801) {
+        /*
+        Check if the 1 <= length <= 800 and
+        the input is not blank
+        */
+        if (messageTextInput.innerText.length > 0 &&
+            messageTextInput.innerText.length < 801 &&
+            messageTextInput.innerText.trim().length > 0) {
             const request = new XMLHttpRequest();
             request.open("POST", messagesEndpoint);
             request.send(JSON.stringify({
