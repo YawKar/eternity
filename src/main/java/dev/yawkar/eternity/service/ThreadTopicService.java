4a52b125a6416d6f164416867318e79dfaea7c5d
yawkar <yawkarpov@gmail.com>
2022-08-20 17:41:11 +0300

    refactor: delete MessageService and move its methods into ThreadTopicService

--- src/main/java/dev/yawkar/eternity/service/ThreadTopicService.java
+++ src/main/java/dev/yawkar/eternity/service/ThreadTopicService.java
@@ -1,7 +1,9 @@
 package dev.yawkar.eternity.service;
 
 import dev.yawkar.eternity.persistence.exception.ThreadNotFoundException;
+import dev.yawkar.eternity.persistence.model.Message;
 import dev.yawkar.eternity.persistence.model.ThreadTopic;
+import dev.yawkar.eternity.persistence.repository.MessageRepository;
 import dev.yawkar.eternity.persistence.repository.ThreadTopicRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,18 +12,32 @@ import java.util.List;
 @Service
 public class ThreadTopicService {
 
-    private final ThreadTopicRepository repository;
+    private final ThreadTopicRepository threadRepository;
+    private final MessageRepository messageRepository;
 
-    public ThreadTopicService(ThreadTopicRepository repository) {
-        this.repository = repository;
+    public ThreadTopicService(
+            ThreadTopicRepository threadRepository,
+            MessageRepository messageRepository) {
+        this.threadRepository = threadRepository;
+        this.messageRepository = messageRepository;
     }
 
     public List<ThreadTopic> get10TopicsSortedByMessagesNumber() {
-        return repository.findTop10ByOrderByMessagesNumberDesc();
+        return threadRepository.findTop10ByOrderByMessagesNumberDesc();
     }
 
     public ThreadTopic getThreadById(long threadId) {
-        return repository.findById(threadId)
-                .orElseThrow(() -> new ThreadNotFoundException("Thread with threadId=%d not found".formatted(threadId)));
+        return threadRepository.findById(threadId).orElseThrow(() -> new ThreadNotFoundException(threadId));
+    }
+
+    public List<Message> getAllMessagesByThreadIdSortedByIdAsc(long threadId) {
+        return messageRepository.findAllByThreadIdOrderByIdAsc(threadId);
+    }
+
+    public void addMessageToThread(Message newMessage) {
+        if (!threadRepository.existsById(newMessage.getThreadId())) {
+            throw new ThreadNotFoundException(newMessage.getThreadId());
+        }
+        messageRepository.save(newMessage);
     }
 }
