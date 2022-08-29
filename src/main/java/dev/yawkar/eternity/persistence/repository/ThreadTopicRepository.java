1648998677b9f826b88d0aae7a517cd0a6528a1d
yawkar <yawkarpov@gmail.com>
2022-08-29 14:27:05 +0300

    feat: add findTop10ThreadByMessagesLastWeek method with nativeQuery
    it selects top 10 threads sorted by number of messages posted in the thread within the last 7 days

--- src/main/java/dev/yawkar/eternity/persistence/repository/ThreadTopicRepository.java
+++ src/main/java/dev/yawkar/eternity/persistence/repository/ThreadTopicRepository.java
@@ -2,7 +2,18 @@ package dev.yawkar.eternity.persistence.repository;
 
 import dev.yawkar.eternity.persistence.model.ThreadTopic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ThreadTopicRepository extends JpaRepository<ThreadTopic, Long> {
+
+    @Query(nativeQuery = true, value = "" +
+            "SELECT t.* " +
+            "FROM thread_topics t " +
+            "GROUP BY t.id " +
+            "ORDER BY (SELECT count(*) FROM messages m WHERE m.thread_id = t.id AND m.timestamp BETWEEN now() - INTERVAL '7 DAYS' AND now()) DESC " +
+            "LIMIT 10")
+    List<ThreadTopic> findTop10ThreadByMessagesLastWeek();
 
 }
