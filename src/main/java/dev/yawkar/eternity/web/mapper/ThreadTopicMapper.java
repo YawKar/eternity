ed05fa96598bb195fc0d6fa93e2e40276c6bfda5
yawkar <yawkarpov@gmail.com>
2022-08-30 12:28:32 +0300

    feat: add toEntity() method into ThreadTopicMapper & addNewThreadTopic method into ThreadTopicService
    now it is possible to create new threads through ThreadTopicService

--- src/main/java/dev/yawkar/eternity/web/mapper/ThreadTopicMapper.java
+++ src/main/java/dev/yawkar/eternity/web/mapper/ThreadTopicMapper.java
@@ -1,11 +1,18 @@
 package dev.yawkar.eternity.web.mapper;
 
 import dev.yawkar.eternity.persistence.model.ThreadTopic;
+import dev.yawkar.eternity.web.dto.request.NewThreadDTO;
 import dev.yawkar.eternity.web.dto.response.ThreadTopicDTO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ThreadTopicMapper {
+
+    public ThreadTopic toEntity(NewThreadDTO newThreadDTO) {
+        return new ThreadTopic()
+                .setTitle(newThreadDTO.getTitle())
+                .setDescription(newThreadDTO.getDescription());
+    }
 
     public ThreadTopicDTO toDTO(ThreadTopic threadTopic) {
         return new ThreadTopicDTO()
