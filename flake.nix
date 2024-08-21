67a2f0d71b15ef9b5a62df36e542a4edc78e45fc
YawKar <yawkarpov@gmail.com>
2024-08-21 22:33:50 +0300

    nixify

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "eternity";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { /*self,*/ nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.temurin-bin-17
+          ];
+          shellHook = ''
+            export PS1="(eternity)$PS1"
+          '';
+        };
+      }
+    );
+}
+
